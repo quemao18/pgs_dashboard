9c5b0889036b387384166fcaf913d6ad55a013b8
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2019-12-12 12:52:13 -0400

    na

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,8 +2,6 @@
 export var app='PGS Health';
 export var url='#'
 export var version: string="2.0";
-//export var apiUrl='http://localhost:8080/npeht_api/api';
-//export var apiUrl='http://144.217.255.53/npeht_api/api';
 export var nameKeyApi='X-API-KEY';
 export var keyApi='LCiAE8C30IQIuG8s27gtU0b6eZ7hlXzSKBcqFaes';
 export var apiError='No hay respuesta del servidor. Intente nuevamente.';
